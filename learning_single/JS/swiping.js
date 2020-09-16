24a288cdd2e6ff5b24772d170c7778f0729b6c1c
sy7777 <piecesy7@gmail.com>
2020-09-16 17:53:03 +1000

    swipe direction

--- learning_single/JS/swiping.js
+++ learning_single/JS/swiping.js
@@ -1,30 +1,108 @@
+var body = document.querySelector("body");
+body.ontouchstart = function(event){
+    // console.log(event,"触碰时间开始")
+}
+body.ontouchmove = function(event){
+    // console.log(event,"触碰移动开始")
+}
+body.ontouchend = function(event){
+    // console.log(event,"触碰结束了")
+}
+//1. 定义自定事件
+//2. 设定四个方向
+//
+//
+//swipe.addEvent（“自定义事件名称”，事件触发执行函数（先不执行，在某个地方储存起来））
+//swipe.emit（“自定义事件名称”，传入事件触发的信息）
 var swipe = {
     eventAll:{
-        //“自定义的事件名称”：['事件要出发执行的函数1', "事件要出发执行的函数2"]
-        
+        //“自定义的事件名称”：['事件要出发执行的函数1', "事件要出发执行的函数2"……]       
     },
-/*     init: function(dom){
-        dom.eventAll = {},
-        dom.addEvent = this.addEvent
-    } */
 
+    init: function(dom){
+        dom.eventAll = {},
+        dom.addEvent = this.addEvent;
+        dom.emit = this.emit;
+        dom.removeEvent = this.removeEvent;
+        dom.touchesData= {};
+    },
     addEvent:function(eventName, callbackFn){
+        // this.init(dom);
         if (this.eventAll[eventName] == undefined){
-            eventAll = []
-            console.log(this.eventAll);  
+            this.eventAll[eventName] = []
+            //console.log(this.eventAll);  
             //console.log(this.eventName);  
             console.log(eventName);  
-        }
-        eventAll[eventName].push(callbackFn);
+        }      
+        this.eventAll[eventName].push(callbackFn);
+        console.log(this.eventAll); 
     },
     emit: function(eventName, eventMsg){
         this.eventAll[eventName].forEach(function(item, i){
+            //console.log(item)
             item(eventMsg)
+        })
+    },
+    removeEvent:function(eventName, callbackFn){
+        var that = this;
+        this.eventAll[eventName].forEach(function(item, i){
+            if(item == callbackFn){
+                //这个this是指function针对的对象，应该是item，不是wipe的对象
+                that.eventAll[eventName].splice(i,1)
+            }
         })
     }
 }
-swipe.addEvent("swipeLeft", function(eventMsg){
+swipe.init(body);
+body.addEvent("swipeLeft", function(eventMsg){
     console.log("swipeLeft");
     console.log(eventMsg);
 })
-swipe.emit("swipeLeft","这是向左滑动事件")
+body.emit("swipeLeft","这是向左滑动事件");
+body.addEventListener("touchstart",function(e){
+    console.log(e,"start");
+    this.touchesData.startX = e.touches[0].pageX;
+    this.touchesData.startY = e.touches[0].pageY;
+})
+body.addEventListener("touchmove",function(e){
+    // console.log(e);
+    this.touchesData.endX = e.touches[0].pageX;
+    this.touchesData.endY = e.touches[0].pageY;
+})
+// 结束事件没有pageXY的位置，只能通过移动来获取
+body.addEventListener("touchend",function(e){
+    console.log(e,"end");
+    
+    var x = this.touchesData.endX - this.touchesData.startX;
+    var y = this.touchesData.endY - this.touchesData.startY;
+
+    if((Math.abs(x)> Math.abs(y)) && Math.abs(x)>100){
+        if(x>0){
+            console.log("向右滑");
+        }else{
+            console.log("向左滑");
+        }
+    }else if((Math.abs(x)< Math.abs(y)) && Math.abs(y)>100){
+        if(y>0){
+            console.log("向下滑");
+        }else{
+            console.log("向上滑");
+        }
+    }
+})
+
+/* body.addEventListener("touchstart",function(e){
+    console.log(e);
+}) */
+/* swipe.addEvent(body,"swipeLeft", function(eventMsg){
+    console.log("swipeLeft");
+    console.log(eventMsg);
+})
+var fn = function(eventMsg){
+    console.log("swipeRight");
+    console.log(eventMsg);
+}
+
+swipe.addEvent("swipeLeft",fn)
+swipe.removeEvent("swipeLeft",fn)
+swipe.emit("swipeLeft","这是向左滑动事件") */
