c622a5da10d33558561bcbb5bc9e24fc10d0e801
sy7777 <piecesy7@gmail.com>
2020-09-30 23:39:36 +1000

    render music

--- ES6/getAjax.js
+++ ES6/getAjax.js
@@ -9,7 +9,8 @@ function getAjax(httpUrl){
         xhr.onreadystatechange = function(){
             if(xhr.status == 200 && xhr.readyState == 4){
                 //5. 处理获取得到的数据
-                res(xhr);
+                const DATA = JSON.parse(xhr.response)
+                res(DATA);
             }else if(!xhr.status == 200 && xhr.readyState == 4){
                 //6. 处理失败结果
                 rej(xhr);
@@ -19,5 +20,51 @@ function getAjax(httpUrl){
         //4.发送数据
         xhr.send()
     })
-    console.log(ajaxP);
 }
+
+
+async function test() {
+    const http = `https://api.apiopen.top/getJoke?page=1&count=5&type-video`;
+    const result = await getAjax(http);
+   
+}
+test()
+
+
+async function getMusic() {
+    const http = `https://api.apiopen.top/musicRankings`;
+    const result = await getAjax(http);
+    console.log(result.result);
+    const newRes = result.result;
+    for(let i = 0;i<newRes.length;i++){
+       
+        const content = result.result;
+        const typeName = newRes[i].name;
+        console.log(content);
+        const musicName = content[i].album_title;
+        console.log(content);
+        // $('body').append(`<h2>${typeName}</h2>`)
+        for (let j = 0; j < content.length; j++) {
+            // 同步的，很慢
+            // const musicDetail = await getAjax(`https://api.apiopen.top/musicDetails?id=${content['song_id']}`);
+            
+            // promise操作
+            /* const http = `https://api.apiopen.top/musicDetails?id=${content['song_id']}`
+            const musicDetail = getAjax(http).then((res,rej)=>{
+                console.log(musicDetail);
+                // 渲染操作
+            }); */
+
+            let fn =async function() {
+                const http = `https://api.apiopen.top/musicDetails?id=${content['song_id']}`
+                const musicDetail = await getAjax(http)
+                // console.log(musicDetail);
+                $('body').append(`<h4>${musicName}:音乐${content[j]}</h4>`)
+            }
+            fn()
+           
+        }
+    }
+}
+getMusic()
+// 获取音乐排行榜
