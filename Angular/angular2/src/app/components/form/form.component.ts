0b807bfa36919f9a31edc7f09dffa693b86d75a9
sy7777 <piecesy7@gmail.com>
2020-10-06 15:47:23 +1000

    form双向绑定事件

--- Angular/angular2/src/app/components/form/form.component.ts
+++ Angular/angular2/src/app/components/form/form.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public peopleInfo:any={
+    username: '',
+    gender: '',
+    cities:['北京','上海', '深圳', '成都', '广州'],
+    city:'',
+    hobbies:[{
+      title:"吃饭",
+      checked:false
+    },{
+      title:"睡觉",
+      checked:false
+    },{
+      title:"打豆豆",
+      checked:false
+    }],
+    notes:''
+  }
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  doSubmit(){
+    console.log(this.peopleInfo);
+
   }
 
 }
