094fa5605a22ff6e6666b206e258e093368d93a7
sy7 <piecesy7@gmail.com>
2021-03-16 10:03:43 +1000

    refined remove function

--- test/ensure.js
+++ test/ensure.js
@@ -1,12 +1,16 @@
 function ensure(value){
-    if(!value || typeof value === "undefined"){
+    // console.log(typeof value);
+    if(!value && typeof value !== "boolean" && typeof value !== "number" && value !== ""){
         throw Error("Your value is null or undefined!")
     }else{
         return value;
     }
+    // /??看前面的是不是null and undefined
+    // return value ?? 
 }
 try{
-    console.log(ensure(undefined));
+    // console.log(ensure(NaN));
 }catch(err){
     console.log(err);
 }
+console.log(typeof NaN);
